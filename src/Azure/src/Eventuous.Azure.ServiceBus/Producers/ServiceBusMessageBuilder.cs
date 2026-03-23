db47d9ba84ced291f20a0a3e4fabbcc29442104e
Mikey <quezlatch@gmail.com>
2026-03-23 16:42:09 +0000

    add ScheduledEnqueueTime property to ServiceBusProduceOptions

--- src/Azure/src/Eventuous.Azure.ServiceBus/Producers/ServiceBusMessageBuilder.cs
+++ src/Azure/src/Eventuous.Azure.ServiceBus/Producers/ServiceBusMessageBuilder.cs
@@ -36,7 +36,8 @@ class ServiceBusMessageBuilder(
             CorrelationId = message.Metadata?.GetCorrelationId(),
             To = metadata?.GetValueOrDefault(attributes.To, options?.To)?.ToString(),
             ReplyTo = metadata?.GetValueOrDefault(attributes.ReplyTo, options?.ReplyTo)?.ToString(),
-            ReplyToSessionId = options?.ReplyToSessionId
+            ReplyToSessionId = options?.ReplyToSessionId,
+            ScheduledEnqueueTime = options?.ScheduledEnqueueTime ?? default
         };
 
         // We set the SessionId only when a value is present because
